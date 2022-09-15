79e4ec690135c10b3b5ee973759444610b7ea3cf
Andrew Craig <56005072+AndrewCraig101@users.noreply.github.com>
2022-09-15 16:28:43 -0400

    Javascript added

---
+++ script/script.js
@@ -0,0 +1,54 @@
+console.log('hello')
+
+
+// PRINTING CUSTOM NAMES
+
+function insertClientName() {
+
+    let clientName1 = "Amy Jones"
+    let clientName2 = "Mark Hamil"
+    let clientName3 = "Dan Lorenzo"
+
+    // let clientNameEl = document.querySelectorAll('clientNameSpan')
+
+    let messages = document.querySelectorAll(".clientNameSpan");
+
+    messages.forEach(message => {
+        message.textContent = clientName1
+    });
+
+
+
+    clientNameEl.textContent = clientName1  
+}
+
+function insertExecName() {
+
+    let execName1 = "Jessica Dunn"
+    let execName2 = "Brian Cray"
+
+    let execNameEl = document.getElementById('execNameSpan')
+    execNameEl.textContent = execName1
+
+}
+
+insertClientName()
+insertExecName()
+
+let englishVar = "English"
+let frenchSymbol = "French"
+
+function printLanguage() {
+    let englishVar = "English"
+    let frenchVar = "French"
+
+    languageEl = document.getElementById('languageSpan')
+    languageEl.textContent = frenchVar
+}
+
+printLanguage()
+
+
+
+
+
